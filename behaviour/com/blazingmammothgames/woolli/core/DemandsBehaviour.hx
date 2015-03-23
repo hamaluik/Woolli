cb0675ffaf2e4479061fecd38bbc5303c82b4e38
Kenton Hamaluik <kenton@hamaluik.com>
2015-03-22 20:42:08 -0600

    Fleshed out Demands behaviour tests

--- behaviour/com/blazingmammothgames/woolli/core/DemandsBehaviour.hx
+++ behaviour/com/blazingmammothgames/woolli/core/DemandsBehaviour.hx
@@ -1,5 +1,9 @@
 package com.blazingmammothgames.woolli.core;
 
+import com.blazingmammothgames.woolli.core.Component;
+import com.blazingmammothgames.woolli.core.WoolliException;
+import com.blazingmammothgames.woolli.mockups.*;
+
 import buddy.*;
 using buddy.Should;
 
@@ -26,16 +30,57 @@ class DemandsBehaviour extends BuddySuite
 			it("should be able to chain-construct using lacks", {
 				demands.lacks(Component).should.be(demands);
 			});
-			it("should match an entity with at least one matching component");
-			it("shouldn't match an entity that contains an undesired component");
-			it("shouldn't be able to lack a component that it already requires");
-			it("shouldn't be able to require a component that it already lacks");
-			it("should throw an exception if you try to add the same component twice");
-			it("should thrown an exception if you try to restrict the same component twice");
-			it("should be able to match any entity when there are no requirements whatsoever");
-			it("shouldn't match an entity if the entity is missing a required component");
-			it("shouldn't match an entity if the entity has a restricted component");
-			it("shouldn't match an entity if the entity's component has been disabled");
+			it("should match an entity with at least one matching component", {
+				var e:Entity = new Entity();
+				e.addComponent(new MockComponentA());
+				demands.requires(MockComponentA);
+				demands.matchesEntity(e).should.be(true);
+			});
+			it("shouldn't match an entity that contains an undesired component", {
+				var e:Entity = new Entity();
+				e.addComponent(new MockComponentA());
+				demands.lacks(MockComponentA);
+				demands.matchesEntity(e).should.be(false);
+			});
+			it("should throw an exception if you try to add the same component twice", {
+				demands.requires(MockComponentA).requires.bind(MockComponentA).should.throwType(WoolliException);
+			});
+			it("should throw an exception if you try to restrict the same component twice", {
+				demands.lacks(MockComponentA).lacks.bind(MockComponentA).should.throwType(WoolliException);
+			});
+			it("should throw an exception if you try to restrict a component that has already been required", {
+				demands.requires(MockComponentA).lacks.bind(MockComponentA).should.throwType(WoolliException);
+			});
+			it("should throw an exception if you try to require a component that has already been restricted", {
+				demands.lacks(MockComponentA).requires.bind(MockComponentA).should.throwType(WoolliException);
+			});
+			it("should be able to match any entity when there are no requirements whatsoever", {
+				var e1:Entity = new Entity();
+				e1.addComponent(new MockComponentA());
+				var e2:Entity = new Entity();
+				demands.matchesEntity(e1).should.be(true);
+				demands.matchesEntity(e2).should.be(true);
+			});
+			it("shouldn't match an entity if the entity is missing a required component", {
+				var e1:Entity = new Entity();
+				e1.addComponent(new Component());
+				demands.requires(MockComponentA);
+				demands.matchesEntity(e1).should.be(false);
+			});
+			it("shouldn't match an entity if the entity has a restricted component", {
+				var e1:Entity = new Entity();
+				e1.addComponent(new MockComponentA());
+				demands.lacks(MockComponentA);
+				demands.matchesEntity(e1).should.be(false);
+			});
+			it("shouldn't match an entity if the entity's component has been disabled", {
+				var e:Entity = new Entity();
+				var c:Component = new Component();
+				e.addComponent(c);
+				demands.requires(Component);
+				c.enabled = false;
+				demands.matchesEntity(e).should.be(false);
+			});
 		});
 	}
 }
